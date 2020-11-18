e7a244de00a5675efd9b9e6a7aadca6289b893c9
Alexandr Khvostov <san987873@yandex.ru>
2020-11-18 17:49:23 +0300

    Trying to make things work

--- pipelines/dev/init.sh
+++ pipelines/dev/init.sh
@@ -1,17 +1,28 @@
-sudo ./packages_install.sh
-sudo groupadd docker
-sudo usermod -aG docker ubuntu
-iptables -P FORWARD ACCEPT
+pushd $HOME
+    sudo ITMO-Psychopass-Team/pipelines/dev/packages_install.sh
+    
+    sudo groupadd docker
+    sudo usermod -aG docker ubuntu
+    iptables -P FORWARD ACCEPT
 
-microk8s enable dashboard dns
+    microk8s enable dashboard dns
 
-mkdir /home/ubuntu/.kube
-microk8s config > /home/ubuntu/.kube/config
-cat /home/ubuntu/.kube/config
+    mkdir .kube
+    microk8s config > .kube/config
+    echo -e "\nCopy and paste configs to your \$HOME/.kube/config in order to use kubectl from host machine"
+    cat .kube/config
+    echo ""
 
-sudo microk8s kubectl get deployment -nkube-system kubernetes-dashboard -o yaml > /tmp/kubernetes_dashboard.yaml
-cd configure_dashboard > /dev/null
-    sudo ./configure.sh
-cd .. > /dev/null
-sudo microk8s kubectl apply -f /tmp/kubernetes_dashboard.yaml
-rm /tmp/kubernetes_dashboard.yaml
+    kubectl get deployment -nkube-system kubernetes-dashboard -o yaml > /tmp/kubernetes_dashboard.yaml
+    pushd ITMO-Psychopass-Team/pipelines/dev/configure_dashboard
+        sudo ./configure.sh
+    popd > /dev/null
+    kubectl apply -f /tmp/kubernetes_dashboard.yaml
+    rm /tmp/kubernetes_dashboard.yaml
+
+    ssh-keygen -t rsa -f $HOME/.ssh/id_rsa -q -P "" <<< y >> /dev/null
+    cat .ssh/id_rsa.pub > authorized_keys
+    echo -e "\nUse the following key to access dev machine through ssh"
+    cat  .ssh/id_rsa
+    echo ""
+popd > /dev/null
