e9c7c585d8e0d0ee6a30556ba6e81809bd22aef1
Alexandr Khvostov <San987873@yandex.ru>
2020-11-18 20:52:21 +0300

    update git

--- pipelines/dev/configure_git.sh
+++ pipelines/dev/configure_git.sh
@@ -1,3 +1,4 @@
-echo "machine github.com login $GIT_LOGIN password $GIT_KEY" > ~/_netrc
+echo "machine github.com login $GIT_LOGIN password $GIT_KEY" > ~/.netrc
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_USERNAME"
+echo "git configured"
