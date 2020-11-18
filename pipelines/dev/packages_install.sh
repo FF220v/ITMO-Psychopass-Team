b9498f0a9b3338bc61018972b9abe64885a74fb3
Alexandr Khvostov <san987873@yandex.ru>
2020-11-18 19:33:57 +0300

    update

--- pipelines/dev/packages_install.sh
+++ pipelines/dev/packages_install.sh
@@ -1,3 +1,6 @@
+sudo snap install microk8s --classic --channel=1.19/stable
+sudo install kubectl --classic
+
 apt update
 apt install docker.io -y
 apt install jq -y
--- pipelines/dev/snaps_install.sh
+++
@@ -1,2 +0,0 @@
-snap install microk8s --classic --channel=1.19/stable
-snap install kubectl --classic
