433081cd07416a3868c06bb5fbec5be2a020d247
Alexandr Khvostov <san987873@yandex.ru>
2020-11-18 19:46:12 +0300

    update

--- pipelines/dev/configure_perms.sh
+++ pipelines/dev/configure_perms.sh
@@ -1,6 +1,6 @@
 groupadd docker
-usermod -aG docker $USER
-usermod -aG microk8s $USER
-chown -f -R $USER ~/.kube
-chown $USER /tmp
-chown $USER /run
+usermod -aG docker ubuntu
+usermod -aG microk8s ubuntu
+chown -f -R ubuntu ~/.kube
+chown ubuntu /tmp
+chown ubuntu /run
