a1160d0083c2d38080c0c482253322a2b7e71644
Alexandr Khvostov <san987873@yandex.ru>
2020-11-21 18:19:27 +0300

    Trying out new projects structure

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ docker volume rm civilla-maven-repo
 docker volume create --name civilla-maven-repo
 docker run -it -v civilla-maven-repo:/root/.m2 civilladev/builder mvn archetype:generate
 
+pushd $HOME/ITMO-Psychopass-Team/scripts/builder > /dev/null
+    ./build.sh
+popd > /dev/null
+
 pushd ~/ITMO-Psychopass-Team/sources/projects/Base > /dev/null
     ./add_to_repo.sh
 popd > /dev/null
