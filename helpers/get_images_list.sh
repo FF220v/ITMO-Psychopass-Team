729d4914042108465e1b8ce2bd02b3b40455514a
Alexandr Khvostov <san987873@yandex.ru>
2020-11-28 04:34:50 +0300

    add devices server

--- helpers/get_images_list.sh
+++ helpers/get_images_list.sh
@@ -12,5 +12,6 @@ cat <<EOF
     "civilla_mongodb_image": "`IMAGE=civilla/mongodb get_image`",
     "civilla_mongodbproxyserver_image": "`IMAGE=civilla/mongodbproxyserver get_image`",
     "civilla_notificationserver_image": "`IMAGE=civilla/notificationserver get_image`"
+    "civilla_devicesserver_image": "`IMAGE=civilla/devicesserver get_image`"
 }
 EOF
