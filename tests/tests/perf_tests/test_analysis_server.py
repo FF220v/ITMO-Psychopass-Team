2153d701976ea5c313c4fbd227543c99361efacf
Alexandr Khvostov <san987873@yandex.ru>
2021-01-17 14:41:15 +0300

    Working tests

--- tests/tests/perf_tests/test_analysis_server.py
+++ tests/tests/perf_tests/test_analysis_server.py
@@ -1,6 +1,7 @@
 import asyncio
 import json
 import time
+from statistics import mean
 
 import aiohttp
 import pytest
@@ -15,8 +16,8 @@ async def setup_test(fill_user_data):  # to avoid insufficient notifications set
 
 async def send_request_with_time_measurement(async_http_client: aiohttp.ClientSession, data):
     start = time.time()
-    await async_http_client.post(f"{get_analysis_server_url()}/analyse", data=json.dumps(data))
-    return time.time() - start
+    resp = await async_http_client.post(f"{get_analysis_server_url()}/analyse", data=json.dumps(data))
+    return resp, time.time() - start
 
 
 @pytest.fixture()
@@ -26,12 +27,27 @@ def analysis_test_data(mongo_client):
 
 
 @pytest.mark.asyncio
-async def test_response_time(mongo_client, async_http_client, analysis_test_data):
-    elapsed = await send_request_with_time_measurement(async_http_client, analysis_test_data)
+async def test_response_time_single_request(mongo_client, async_http_client, analysis_test_data):
+    _, elapsed = await send_request_with_time_measurement(async_http_client, analysis_test_data)
+    print(f"Elapsed: {elapsed}")
+    assert elapsed < 1
 
 
 @pytest.mark.asyncio
 async def test_high_load_response_time_average(mongo_client, async_http_client, analysis_test_data):
     results = await asyncio.gather(*[send_request_with_time_measurement(async_http_client, analysis_test_data)
                                      for i in range(100)])
-    print("hello")
+    elapsed_list = [elapsed for _, elapsed in results]
+    average_elapsed = mean(elapsed_list)
+    print(f"Elapsed: {average_elapsed}")
+    assert average_elapsed < 10
+
+
+@pytest.mark.asyncio
+async def test_high_load_response_time_overload(mongo_client, async_http_client, analysis_test_data):
+    results = await asyncio.gather(*[send_request_with_time_measurement(async_http_client, analysis_test_data)
+                                     for i in range(1000)])
+    elapsed_list = [elapsed for _, elapsed in results]
+    average_elapsed = mean(elapsed_list)
+    print(f"Elapsed: {average_elapsed}")
+    assert average_elapsed < 15
