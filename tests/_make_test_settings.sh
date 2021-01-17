0bb1ba5e063bcfc209209742f1c8ab86eb3e394b
Alexandr Khvostov <san987873@yandex.ru>
2021-01-17 16:19:25 +0300

    update test runners

--- tests/_make_test_settings.sh
+++ tests/_make_test_settings.sh
@@ -4,7 +4,7 @@ cat <<EOF
     "app_hash": "`sops -d secrets/test_app.json | jq -r .$STAGE.app_hash`",
     "connection_string": "`cat tests/conn.json | jq -r .connection_string`",
     "bot_name": "`sops -d secrets/kube_secrets.json | jq -r .$STAGE.bot_name`",
-    "stage": "$STAGE"
+    "stage": "$STAGE",
     "mongo_host": "localhost:27017",
     "devices_server_host": "localhost:30000",
     "kubectl_proxy": "localhost:8002"
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,3 +1,5 @@
+TESTS_DIR=${1:-"tests"}
+
 pushd ~/ITMO-Psychopass-Team/tests
     ./build.sh
     ./make_test_settings.sh
@@ -8,16 +10,14 @@ MONGO_POD=`kubectl get pods -ncivilla-stage --no-headers -o custom-columns=":met
 kubectl port-forward $MONGO_POD -ncivilla-stage --address='0.0.0.0' 27017:27017 > /dev/null &
 KUBECTL_MONGO_FWD_PID=$!
 
-KUBE_PROXY=`kubectl proxy --context microk8s --address='0.0.0.0' --port=8002 --accept-hosts='.*'`
+KUBE_PROXY=`kubectl proxy --context microk8s --address='0.0.0.0' --port=8002 --accept-hosts='.*'` &
 KUBE_PROXT_PID=$!
 
 
 docker run -v ~/ITMO-Psychopass-Team/tests:/src \
            --network="host" \
       	   civilladev/acceptance-tests \
-           bash -c "cd /src && python -m pytest /src/tests -vv $@"
-
+           bash -c "cd /src && python -m pytest /src/$TESTS_DIR -vv"
 
 kill $KUBECTL_MONGO_FWD_PID
-kill $KUBECTL_ANALYSIS_FWD_PID
 kill $KUBE_PROXT_PID
