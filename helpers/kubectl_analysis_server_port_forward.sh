74bcd3894ab884c74ec39df403fcc078e73b8973
Alexandr Khvostov <san987873@yandex.ru>
2021-01-17 15:59:10 +0300

    update addresses, settings.json

--- helpers/kubectl_analysis_server_port_forward.sh
+++
@@ -1,2 +0,0 @@
-POD=`kubectl get pods -ncivilla-stage --no-headers -o custom-columns=":metadata.name" | grep analysisserver-`
-kubectl port-forward $POD -ncivilla-stage --address='0.0.0.0' 27017:27017
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -8,10 +8,6 @@ MONGO_POD=`kubectl get pods -ncivilla-stage --no-headers -o custom-columns=":met
 kubectl port-forward $MONGO_POD -ncivilla-stage --address='0.0.0.0' 27017:27017 > /dev/null &
 KUBECTL_MONGO_FWD_PID=$!
 
-POD=`kubectl get pods -ncivilla-stage --no-headers -o custom-columns=":metadata.name" | grep analysisserver-`
-kubectl port-forward $POD -ncivilla-stage --address='0.0.0.0' 80:8051 > /dev/null &
-KUBECTL_ANALYSIS_FWD_PID=$!
-
 KUBE_PROXY=`kubectl proxy --context microk8s --address='0.0.0.0' --port=8002 --accept-hosts='.*'`
 KUBE_PROXT_PID=$!
 
